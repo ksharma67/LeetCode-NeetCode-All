2af05ed23564ade53d589c9916cb9ee044af671d
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2023-06-19 11:17:18 -0400

    Create 2542. Maximum Subsequence Score.py

---
+++ Heap or Priority Queue/2542. Maximum Subsequence Score.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        res, prefixSum, minHeap = 0, 0, []
+        
+        for a, b in sorted(list(zip(nums1, nums2)), key=itemgetter(1), reverse=True):
+            prefixSum += a
+            heappush(minHeap, a)
+            if len(minHeap) == k:
+                res = max(res, prefixSum * b)
+                prefixSum -= heappop(minHeap)                               
+        
+        return res
