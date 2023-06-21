477ee671e457ab155a1130cbba9b74caab37c651
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2023-06-21 11:04:16 -0400

    Create 1822. Sign of the Product of an Array.py

---
+++ Array and Hashing/1822. Sign of the Product of an Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        sign = 1
+
+        for i in nums:
+
+            if i == 0:
+                return 0
+
+            if i < 0:
+                sign = -sign
+        
+        return sign
